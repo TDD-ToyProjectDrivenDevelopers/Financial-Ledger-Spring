a853687d51bcbbe1db25210e204ca58ed188a10f
CokeLee777 <akak640@kyonggi.ac.kr>
2022-06-26 14:31:01 +0900

    Project initialize

---
+++ src/main/java/gubun/financialledger/FinancialLedgerApplication.java
@@ -0,0 +1,13 @@
+package gubun.financialledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialLedgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialLedgerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gubun/financialledger/FinancialLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package gubun.financialledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialLedgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
