af034968d9dfbc0da762dc2b130030cbd4282646
CokeLee777 <akak640@kyonggi.ac.kr>
2022-07-11 15:41:32 +0900

    feat(OAuth API): Add OAuth info in yml file and setting OAuth user info class for OAuth attributes
    1. yml 파일에 OAuth 인증, 인가, 사용자 정보를 받아오기 위한 정보 추가
    2. 사용자 정보를 받아와서 객체에 저장하기 위한 클래스 추가
    
    #20

---
+++ src/main/java/gubun/financialledger/user/oauth/provider/OAuth2UserInfo.java
@@ -0,0 +1,11 @@
+package gubun.financialledger.user.oauth.provider;
+
+import java.util.Map;
+
+public interface OAuth2UserInfo {
+    Map<String, Object> getAttributes();
+    String getProviderId();
+    String getProvider();
+    String getEmail();
+    String getName();
+}
